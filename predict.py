0cc4cc38f7012ef945d27c1e5581d80d005fb232
Denis K <denis911@gmail.com>
2025-10-26 21:37:04 +0100

    predict.py - to download model from pickle local file, run fastAPI and makes predictions online

---
+++ predict.py
@@ -0,0 +1,75 @@
+import pickle
+from typing import Literal
+from pydantic import BaseModel, Field
+
+
+from fastapi import FastAPI
+import uvicorn
+
+
+
+class Customer(BaseModel):
+    gender: Literal["male", "female"]
+    seniorcitizen: Literal[0, 1]
+    partner: Literal["yes", "no"]
+    dependents: Literal["yes", "no"]
+    phoneservice: Literal["yes", "no"]
+    multiplelines: Literal["no", "yes", "no_phone_service"]
+    internetservice: Literal["dsl", "fiber_optic", "no"]
+    onlinesecurity: Literal["no", "yes", "no_internet_service"]
+    onlinebackup: Literal["no", "yes", "no_internet_service"]
+    deviceprotection: Literal["no", "yes", "no_internet_service"]
+    techsupport: Literal["no", "yes", "no_internet_service"]
+    streamingtv: Literal["no", "yes", "no_internet_service"]
+    streamingmovies: Literal["no", "yes", "no_internet_service"]
+    contract: Literal["month-to-month", "one_year", "two_year"]
+    paperlessbilling: Literal["yes", "no"]
+    paymentmethod: Literal[
+        "electronic_check",
+        "mailed_check",
+        "bank_transfer_(automatic)",
+        "credit_card_(automatic)",
+    ]
+    tenure: int = Field(..., ge=0)
+    monthlycharges: float = Field(..., ge=0.0)
+    totalcharges: float = Field(..., ge=0.0)
+
+
+class PredictResponse(BaseModel):
+    churn_probability: float
+    churn: bool
+
+
+app = FastAPI(title="customer-churn-prediction")
+
+with open('model.bin', 'rb') as f_in:
+    pipeline = pickle.load(f_in)
+
+
+def predict_single(customer):
+    result = pipeline.predict_proba(customer)[0, 1]
+    return float(result)
+
+
+@app.post("/predict")
+def predict(customer: Customer) -> PredictResponse:
+    prob = predict_single(customer.model_dump())
+
+    return PredictResponse(
+        churn_probability=prob,
+        churn=prob >= 0.5
+    )
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9696)
+
+# run docs in web browser http://localhost:9696/docs first - 
+# it will open a web page with API call description - 
+# so I can edit POST JSON parameters online - say select tenure etc..
+# and get an online prediction from model in manual mode:
+# {
+#   "churn_probability": 0.42227630449947207,
+#   "churn": false
+# }
+
